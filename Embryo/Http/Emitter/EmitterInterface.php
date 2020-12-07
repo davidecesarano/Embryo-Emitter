104450f4128deedaa59e9ada2db2b83d2af6d3fd
Davide Cesarano <Dav.Cesarano@unipegaso.local>
2020-12-07 12:35:52 +0100

    phpstan

--- Embryo/Http/Emitter/EmitterInterface.php
+++ Embryo/Http/Emitter/EmitterInterface.php
@@ -1,10 +1,21 @@
 <?php 
 
+    /**
+     * EmitterInterface
+     * 
+     * @author Davide Cesarano <davide.cesarano@unipegaso.it>
+     * @link   https://github.com/davidecesarano/embryo-emitter
+     */
+
     namespace Embryo\Http\Emitter;
 
     use Psr\Http\Message\ResponseInterface;
 
     interface EmitterInterface
     {
+        /**
+         * @param ResponseInterface $response 
+         * @return mixed
+         */
         public function emit(ResponseInterface $response);
     }
